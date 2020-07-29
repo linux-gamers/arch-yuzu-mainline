52f09faef6345b6f6d6dfc2afc6d448e1f4ad51c
Linux Gamers <linuxgamers@protonmail.com>
2020-07-29 11:21:25 -0300

    Test

--- .circleci/check_new_release.py
+++ .circleci/check_new_release.py
@@ -8,32 +8,33 @@ published_req = session.get('https://{}@api.github.com/repos/yuzu-emu/yuzu-mainl
 
 if published_req.status_code == 200:
     published_releases = published_req.json()
+    print(published_releases[0]['tag_name'].split('mainline-')[1])
 else:
     exit()
 
-failed_req = session.get('https://raw.githubusercontent.com/linux-gamers/arch-yuzu-mainline/master/builds_failed.txt')
-failed_tags = {t.strip() for t in failed_req.text.split('\n') if t} if failed_req.status_code == 200 else set()
-
-if published_releases[0]['tag_name'].split('mainline-')[1] in failed_tags:
-    exit()
-
-arch_rel_req = session.get('https://{}@api.github.com/repos/linux-gamers/arch-yuzu-mainline/releases/latest'.format(os.getenv('GITHUB_TOKEN')))
-
-if arch_rel_req.status_code == 200:
-    arch_latest_release = 'mainline-{}'.format(arch_rel_req.json()['tag_name'])
-else:
-    exit()
-
-if published_releases[0]['tag_name'] == arch_latest_release:
-    exit()
-
-
-all_not_published = []
-
-for r in published_releases:
-    if r['tag_name'] > arch_latest_release and r['tag_name'] not in failed_tags:
-        all_not_published.append(r['tag_name'])
-    else:
-        break
-
-print(all_not_published[-1].split('mainline-')[1])
+# failed_req = session.get('https://raw.githubusercontent.com/linux-gamers/arch-yuzu-mainline/master/builds_failed.txt')
+# failed_tags = {t.strip() for t in failed_req.text.split('\n') if t} if failed_req.status_code == 200 else set()
+#
+# if published_releases[0]['tag_name'].split('mainline-')[1] in failed_tags:
+#     exit()
+#
+# arch_rel_req = session.get('https://{}@api.github.com/repos/linux-gamers/arch-yuzu-mainline/releases/latest'.format(os.getenv('GITHUB_TOKEN')))
+#
+# if arch_rel_req.status_code == 200:
+#     arch_latest_release = 'mainline-{}'.format(arch_rel_req.json()['tag_name'])
+# else:
+#     exit()
+#
+# if published_releases[0]['tag_name'] == arch_latest_release:
+#     exit()
+#
+#
+# all_not_published = []
+#
+# for r in published_releases:
+#     if r['tag_name'] > arch_latest_release and r['tag_name'] not in failed_tags:
+#         all_not_published.append(r['tag_name'])
+#     else:
+#         break
+#
+# print(all_not_published[-1].split('mainline-')[1])
